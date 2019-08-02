c56fae729c97910f9d15be35f4a29305faa12bb6
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-08-02 14:53:15 +0100

    wip lol

--- q2-schedule/scheduler.rb
+++ q2-schedule/scheduler.rb
@@ -7,37 +7,17 @@
 #   * Sessions are mostly "lesson 1", "lesson 2" or "lesson 3".
 #   * Students can mostly attend all three sessions
 
-
-# Data I'm missing at the moment:
-# Which rooms do these sessions happen in?
-#   12th AM
-#   13th PM
-#   14th AM
-#   15th AM
-#   15th PM
-#   19th AM
-#   19th PM
-#   23rd AM
-#   23rd PM
-#   (Bank Holiday week) 27th AM
-#   27th PM
-#   27th AM
-#   28th AM
-#   28th PM
-#   29th AM
-#   29th PM
-#   30th AM
-#   30th PM
-
 class Session
-  attr_reader :name
-  def initialize(name)
+  attr_reader :name, :room, :capacity
+  def initialize(name, room, capacity)
     @name = name
+    @room = room
+    @capacity = capacity
   end
 end
 
 class Student
-  attr_reader :email, :type, :job_role, :experience
+  attr_reader :email, :type, :job_role, :experience, :availability
   def initialize(email, type, job_role, experience, availability)
     @email = email
     @type = type
@@ -47,19 +27,43 @@ class Student
   end
 end
 
+room_sizes = {
+  "blue" => 10,
+  "orange" => 10,
+  "social space" => 15,
+}
+
 require 'csv'
 
 attendees_raw = CSV.read("downloads/Learn to code at GDS (Q2) (Responses) - Form responses 1.csv")
 student_availability_raw = CSV.read("downloads/Learn to code at GDS (Q2) (Responses) - Form responses 2.csv")
+session_rooms = CSV.read("downloads/Rooms for learn to code Q2 - Sheet1.csv").map{|row|row.map{|cell|cell.strip}}
 
-# Availability should be a map of email to list of lessons
 availability = Hash[student_availability_raw
   .select{|row| row[1].include? "@"}
-  .map{|row| [row[1], (row[2]||'').split(',').map{|x| Session.new(x)}]}]
+  .map{|row| [row[1], (row[2]||'').split(',').map{|x|x.strip}]}]
 
 students = attendees_raw.select { |row|
   row[2] == "new student" || row[2] == "returning student from the Q1 pilot lessons"
 }.map { |row|
   Student.new(row[1], row[2], row[3], row[4], availability[row[1]])
+}
+
+baduns = students.select {|s| !s.availability.nil? && s.availability != [] && s.type == "new student"}.reject { |s|
+  (s.availability.include?('12th AM') || s.availability.include?('13th PM') || s.availability.include?('14th AM')) &&
+  (s.availability.include?('15th AM') || s.availability.include?('15th PM')) &&
+  (s.availability.include?('19th PM') || s.availability.include?('27th PM'))
+}
+
+baduns.each { |s|
+  printf "#{s.email}	"
+  session_rooms.select{|x|x[1] == "social space"}.each{ |x|
+    if s.availability.include?(x[0])
+      printf "Yes	"
+    else
+      printf "No	"
+    end
+  }
+  puts
 }
 
